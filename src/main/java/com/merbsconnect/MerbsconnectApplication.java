cb604bde81a3487979730ddd6a6cb7826cd388ee
Julius <juliussowah@amalitech.com>
2025-05-08 20:44:06 +0000

    Initial commit

---
+++ src/main/java/com/merbsconnect/MerbsconnectApplication.java
@@ -0,0 +1,13 @@
+package com.merbsconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerbsconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerbsconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merbsconnect/MerbsconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merbsconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerbsconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
