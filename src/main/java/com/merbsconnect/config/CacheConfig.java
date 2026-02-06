fe14aa545a78efa74738c720e59f9e1213bfc4c1
Gifted Labs <juliusadjeteysowah@gmail.com>
2026-02-06 01:03:17 +0000

    fix: add missing cache names eventSpeakers and eventGallery to CacheConfig

--- src/main/java/com/merbsconnect/config/CacheConfig.java
+++ src/main/java/com/merbsconnect/config/CacheConfig.java
@@ -16,7 +16,7 @@ public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("events", "registrations", "speakers",
-                "smsTemplates");
+                "smsTemplates", "eventSpeakers", "eventGallery");
         cacheManager.setCaffeine(caffeineCacheBuilder());
         return cacheManager;
     }
